cfe2a4f1d73548f047633833bb506d3a01996604
Leonid Lupachev <llupachev11@gmail.com>
2025-10-08 21:41:32 +0200

    Changed header file

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alisseye <alisseye@student.42.fr>          +#+  +:+       +#+        */
+/*   By: danslav1e <danslav1e@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/07 13:07:18 by alisseye          #+#    #+#             */
-/*   Updated: 2025/10/02 15:41:34 by alisseye         ###   ########.fr       */
+/*   Updated: 2025/10/08 21:40:36 by danslav1e        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 // readline, rl_clear_history, rl_on_new_line,
 // rl_replace_line, rl_redisplay, add_history
-# include <readline/readline.h>
 # include <readline/history.h>
+# include <readline/readline.h>
 
 // printf, perror, strerror
 # include <stdio.h>
@@ -30,7 +30,6 @@
 # include <unistd.h>
 
 // pipe, ioctl, tcsetattr, tcgetattr
-# include <unistd.h>
 # include <sys/ioctl.h>
 # include <termios.h>
 
@@ -45,9 +44,18 @@
 # include <dirent.h>
 
 // stat, lstat, fstat
+# include "../libft/libft.h"
+# include <errno.h>
+# include <fcntl.h> // Библиотеки, которые ты забыл включить
+# include <stdbool.h>
+# include <string.h>
 # include <sys/stat.h>
 
-# include <stdbool.h>
+typedef struct s_env_var
+{
+	char			*key;
+	char			*value;
+}					t_env_var;
 
 typedef enum e_token_type
 {
@@ -63,14 +71,14 @@ typedef enum e_token_type
 	TOKEN_RPAREN,
 	TOKEN_EOF,
 	TOKEN_ERROR,
-}	t_token_type;
+}					t_token_type;
 
 typedef struct s_token
 {
-	t_token_type		type;
-	char				*value;
-	bool				to_expand;
-}	t_token;
+	t_token_type	type;
+	char			*value;
+	bool			to_expand;
+}					t_token;
 
 typedef enum e_redirect_type
 {
@@ -78,14 +86,15 @@ typedef enum e_redirect_type
 	REDIRECT_OUT,
 	REDIRECT_APPEND,
 	REDIRECT_HEREDOC,
-}	t_redirect_type;
+}					t_redirect_type;
 
 typedef struct s_redirect
 {
-	t_redirect_type		type;
-	char				*target;
-	bool				to_expand;
-}	t_redirect;
+	t_redirect_type	type;
+	char			*target;
+	int				heredoc_fd; // готовый fd для чтения heredoc (подготовлен заранее) (я пока что хз для чего конкретно он нужен, но gpt настаивает на его добавлении)
+	bool heredoc_quoted; 		// делимитер был в кавычках ->тело heredoc НЕ расширять bool to_expand;
+}					t_redirect;
 
 typedef enum e_node_type
 {
@@ -94,16 +103,26 @@ typedef enum e_node_type
 	NODE_OR,
 	NODE_AND,
 	NODE_SUBSHELL,
-}	t_node_type;
+}					t_node_type;
 
 typedef struct s_node
 {
 	t_node_type		type;
 	char			**argv;
+	size_t argc; // Количество аргументов
 	t_redirect		*redirects;
+	size_t redirects_count; // Количество редиректов
 	struct s_node	*left;
 	struct s_node	*right;
 	struct s_node	*child;
-}	t_node;
+}					t_node;
+
+typedef struct s_shell_state // Общая структура для отслеживания состояния шела
+{
+	t_node		*token_list;
+	t_env_var	*envp;
+	t_node		*first_node;
+	int			last_exit_code;
+}					t_shell_state;
 
 #endif
